df94fd5caa5ea3cd2877f5941cdff19e21b6aea2
Haozhe Xie <cshzxie@gmail.com>
2016-12-11 16:30:31 +0800

    Rebuild the application using TypeScript.

---
+++ desktop/main.ts
@@ -0,0 +1,64 @@
+/// <reference path="typings/index.d.ts" />
+import electron = require("electron");
+let app = electron.app;
+let dialog = electron.dialog;
+let BrowserWindow = electron.BrowserWindow;
+let Menu = electron.Menu;
+
+// Global reference to the main window, so the garbage collector doesn't close it.
+let mainWindow : Electron.BrowserWindow;
+
+// Opens the main window, with a native menu bar.
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+
+    let menuTemplate = [{
+        label: "File",
+        submenu: [{
+            label: "Open",
+            click: () => {
+                // Do something
+            }
+        }, {
+            label: "Exit",
+            click: () => {
+                app.quit();
+            }
+        }]
+    }];
+    let menu : Electron.Menu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(menu);
+
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+    // mainWindow.webContents.openDevTools();
+
+    mainWindow.on("closed", () => {
+        mainWindow = null;
+    });
+}
+
+// Call 'createWindow()' on startup.
+app.on("ready", () => {
+    createWindow();
+    if (process.argv[1] != "main.js") {
+        mainWindow.webContents.on("did-finish-load", () => {
+            mainWindow.webContents.send("load-file", process.argv[1]);
+        });
+    }
+});
+
+// On OS X it is common for applications and their menu bar to stay active until the user quits explicitly
+// with Cmd + Q.
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit()
+    }
+});
+
+// On OS X it's common to re-create a window in the app when the dock icon is clicked and there are no other
+// windows open.
+app.on("activate", () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
